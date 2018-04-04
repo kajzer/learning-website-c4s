cec7bd64e6376b0788532450b4f3d171f85255e6
Karol Jezierski <kajezierski@wp.pl>
2018-04-04 14:39:24 +0000

    Add google omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,4 +256,8 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  
+  require 'omniauth-google-oauth2'
+  config.omniauth :google_oauth2, '112507625097-hqdq2301dl0nioacmiso0k2tt7md7tbi.apps.googleusercontent.com', 'xWBCNlbEwNH7wSIviQWvBCQp', {access_type: "offline", approval_prompt: ""}
+  
 end
