84b77a9951ec6a05b1e741b0b4aaba0d5e9ddbd4
Karol Jezierski <kajezierski@wp.pl>
2018-04-06 06:29:09 +0000

    Refecotered stripes keys into variables using figaro (should refactor social accounts as well)

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,8 @@
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_32OSbVCQKylGc94zscGE3IlA',
-  :secret_key      => 'sk_test_jdjbGdA3n81fNULpOl8t12O5'
-}
+# Rails.configuration.stripe = {
+#   :publishable_key => 'pk_test_32OSbVCQKylGc94zscGE3IlA',
+#   :secret_key      => 'sk_test_jdjbGdA3n81fNULpOl8t12O5'
+# }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+Stripe.api_key = ENV['STRIPE_SECRET_KEY']
